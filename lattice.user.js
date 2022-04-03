48209cc7a4bdd1a69ed16f1ba621396f2cda694e
siah <67557832+jcb1032@users.noreply.github.com>
2022-04-03 11:54:27 -0400

    Add overlay toggle
    Press SPACE to toggle the overlay on and off.
    The overlay will also be automatically disabled when you are zoomed out.

--- lattice.user.js
+++ lattice.user.js
@@ -12,22 +12,47 @@
 // ==/UserScript==
 
 if (window.top !== window.self) {
-	window.addEventListener(
-		"load",
-		() => {
-			document
-				.getElementsByTagName("mona-lisa-embed")[0]
-				.shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0]
-				.shadowRoot.children[0].appendChild(
-					(function () {
-						const i = document.createElement("img");
-						i.src = "https://raw.githubusercontent.com/jcb1032/greenlattice-place/main/lattice_overlay.png";
-						i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
-						console.log(i);
-						return i;
-					})()
-				);
-		},
-		false
-	);
+  window.addEventListener(
+    "load",
+    () => {
+      document
+        .getElementsByTagName("mona-lisa-embed")[0]
+        .shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0]
+        .shadowRoot.children[0].appendChild(
+          (function () {
+            const i = document.createElement("img");
+            i.src = "https://raw.githubusercontent.com/jcb1032/greenlattice-place/main/lattice_overlay.png";
+            i.style = "position:absolute;left:0;top:0;image-rendering:pixelated;width:2000px;height:1000px;";
+            i.setAttribute("data-display", "1");
+
+            // toggle on key press
+            document.addEventListener("keypress", (e) => {
+              if (e.code.toLowerCase() == "space") {
+                i.style.display = i.getAttribute("data-display") == "1" ? "none" : "block";
+                i.setAttribute("data-display", 1 - parseInt(i.getAttribute("data-display")));
+              }
+            });
+
+            // intercept URL changes
+            window.history.oldReplaceState = window.history.replaceState;
+            window.history.replaceState = function (a, b, c, d) {
+              let coordsEl = document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-coordinates")[0].shadowRoot.children[0];
+              let zoomLevel = coordsEl.textContent.replace(/^ \(\d+,\d+\) (\d\.\d)x $/, "$1");
+
+              // disable when you're zoomed out
+              if (zoomLevel == "0.1" || zoomLevel == "0.2") {
+                i.style.display = "none";
+              } else {
+                if (i.getAttribute("data-display") == "1") i.style.display = "block";
+              }
+
+              window.history.oldReplaceState(a, b, c, d);
+            };
+
+            return i;
+          })()
+        );
+    },
+    false
+  );
 }
